d03eda4bcfda88400bab06ec1ce1141c003134ed
Zachary Farrell <zfarrell0507@gmail.com>
2022-08-02 10:13:40 -0400

    Java Set-up

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-function test
+const list = document.querySelector('div#fruit-list')
+
+fetch('http://localhost:3000/Fruits')
+    .then(resp => resp.json())
+    .then(fruits => {
+        renderFruits(fruits)
+        renderDetails(fruits[0])
+    })
+
+function renderFruits(fruits) {
+    fruits.forEach(fruit => {
+        const name = document.createElement('h2')
+        name.textContent = fruit.name
+        list.append(name)
+        name.addEventListener('click', () => renderDetails(fruit))
+    })
+}
+
+function renderDetails(fruit) {
+    const img = document.querySelector('img#fruit_img')
+    img.src = fruit.image
+    const nameDis = document.querySelector('h2#fruit_name')
+    nameDis.textContent = fruit.name
+    const carbs = document.querySelector('span#carbohydrates')
+    carbs.textContent = fruit.carbohydrates
+}
