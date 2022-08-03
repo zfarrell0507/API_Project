36dda0e79ed95c2a0f9e49199703df7563d18106
Zachary Farrell <zfarrell0507@gmail.com>
2022-08-03 10:04:39 -0400

    Add & remove buttons working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fruitForm = document.getElementById('new-fruit')
 let fruitsArray = []
 let currentFruit = {}
 
-/*const add = document.getElementById('add_btn')
+const add = document.getElementById('add_btn')
 const remove = document.getElementById('remove_btn')
 let totalCarbs = document.getElementById('carbs')
 let totalProtein = document.querySelector('span#proteins')
@@ -14,7 +14,7 @@ let tCarbs = 0
 let tProtein = 0
 let tFat = 0
 let tCal = 0
-let tSugar = 0 */
+let tSugar = 0 
 //const divDetail = document.querySelector('div#fruit-detail')
 
 fetch('http://localhost:3000/Fruits')
@@ -48,31 +48,37 @@ function renderDetails(fruit) {
     fat.textContent = fruit.fat
     const calorie = document.querySelector('span#calories')
     calorie.textContent = fruit.calories
+    console.log(fruit.calories)
     const sugar = document.querySelector('span#sugar')
     sugar.textContent = fruit.sugar
-    //add.addEventListener('click', (fruit) => fruitCombo(fruit))
-    //remove.addEventListener('click', (fruit) => removeFromCombo(fruit))
+    add.addEventListener('click', () => fruitCombo(fruit))
+    remove.addEventListener('click', () => removeFromCombo(fruit))
 }
-/*
+
 function fruitCombo(fruit) {
-    console.log(tCarbs)
-    console.log(fruit.target.carbohydrates.value)
-    tCarbs + fruit.target.carbohydrates
-    totalCarbs.textContent = tCarbs
-    console.log(tCarbs)
-    console.log(totalCarbs)
-    tProtein += parseInt(fruit.target.protein)
-    totalProtein.textContent = tProtein
-    tFat += parseInt(fruit.target.fat)
-    totalFat.textContent = tFat
-    tCal += parseInt(fruit.target.calories)
-    totalCalories.textContent = tCal
-    tSugar += parseInt(fruit.target.sugar)
-    totalSugar.textContent = tSugar
+    tCarbs += fruit.carbohydrates
+    totalCarbs.textContent = tCarbs.toFixed(2)
+    tProtein += fruit.protein
+    totalProtein.textContent = tProtein.toFixed(2)
+    tFat += fruit.fat
+    totalFat.textContent = tFat.toFixed(2)
+    tCal += fruit.calories
+    totalCalories.textContent = tCal.toFixed(2)
+    tSugar += fruit.sugar
+    totalSugar.textContent = tSugar.toFixed(2)
 }
 function removeFromCombo(fruit) {
-    console.log(fruit)
-}*/
+    tCarbs -= fruit.carbohydrates
+    totalCarbs.textContent = tCarbs.toFixed(2)
+    tProtein -= fruit.protein
+    totalProtein.textContent = tProtein.toFixed(2)
+    tFat -= fruit.fat
+    totalFat.textContent = tFat.toFixed(2)
+    tCal -= fruit.calories
+    totalCalories.textContent = tCal.toFixed(2)
+    tSugar -= fruit.sugar
+    totalSugar.textContent = tSugar.toFixed(2)
+}
 
 fruitForm.addEventListener('submit', (e) => {
     e.preventDefault()
